f736a6931542a16deaf6138ba1deafb66a3b329f
David Barcenas <daveepro@outlook.com>
2021-01-16 21:05:26 -0600

    feat(LoginPage): Add SharedPreferences

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutterui/widgets/my_btn.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -35,11 +36,13 @@ class _LoginPageState extends State<LoginPage> {
     return 'Invalid password';
   }
 
-  _submit() {
-   final bool isValid =  _formKey.currentState.validate();
-   if(isValid) {
-
-   }
+  _submit() async {
+    final bool isValid = _formKey.currentState.validate();
+    if (isValid) {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setBool('authenticated', true);
+      Navigator.pushReplacementNamed(context, 'home');
+    }
   }
 
   @override
@@ -48,85 +51,87 @@ class _LoginPageState extends State<LoginPage> {
       child: Scaffold(
           body: GestureDetector(
         onTap: () {
-            final FocusScopeNode focus = FocusScope.of(context);
-            if (!focus.hasPrimaryFocus) {
-              focus.unfocus();
-            }
+          final FocusScopeNode focus = FocusScope.of(context);
+          if (!focus.hasPrimaryFocus) {
+            focus.unfocus();
+          }
         },
         child: Container(
-              color: Colors.transparent,
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              width: double.infinity,
-              height: double.infinity,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                 
-                  children: [
-                    ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 300.0),
-            child: Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    Image.network('https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo4.png'),
-                    SizedBox(height: 40,),
-                    TextFormField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30.0)
-            ),
-            prefixIcon: Container(
-              width: 70.0,
-              height: 40.0,
-              padding: EdgeInsets.all(12.0),
-              child: SvgPicture.asset('assets/icons/email.svg')
-            ),
-              hintText: 'daveepro@outlook.com',
-              labelText: 'Email'),
-          textInputAction: TextInputAction.next,
-          keyboardType: TextInputType.emailAddress,
-          validator: _validateEmail,
-          onFieldSubmitted: (String text) {
-            _focusNodePsswd.nextFocus();
-          },
+          color: Colors.transparent,
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 300.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Image.network(
+                            'https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo4.png'),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30.0)),
+                              prefixIcon: Container(
+                                  width: 70.0,
+                                  height: 40.0,
+                                  padding: EdgeInsets.all(12.0),
+                                  child: SvgPicture.asset(
+                                      'assets/icons/email.svg')),
+                              hintText: 'daveepro@outlook.com',
+                              labelText: 'Email'),
+                          textInputAction: TextInputAction.next,
+                          keyboardType: TextInputType.emailAddress,
+                          validator: _validateEmail,
+                          onFieldSubmitted: (String text) {
+                            _focusNodePsswd.nextFocus();
+                          },
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          focusNode: _focusNodePsswd,
+                          textInputAction: TextInputAction.send,
+                          decoration: InputDecoration(
+                            prefixIcon: Container(
+                                width: 70.0,
+                                height: 40.0,
+                                padding: EdgeInsets.all(12.0),
+                                child: SvgPicture.asset(
+                                    'assets/icons/padlock.svg')),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30.0)),
+                            hintText: '*******',
+                            labelText: 'Contraseña',
+                          ),
+                          validator: _validatePsswd,
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        MyBtn(
+                          label: 'Ingresar',
+                          fullWidth: true,
+                          onPressed: _submit,
+                          background: Color(0xff304ffe),
+                          textColor: Colors.white,
+                        ),
+                      ],
                     ),
-                    SizedBox(
-          height: 20.0,
-                    ),
-                    TextFormField(
-          obscureText: true,
-          focusNode: _focusNodePsswd,
-          textInputAction: TextInputAction.send,
-          decoration: InputDecoration(
-            prefixIcon: Container(
-              width: 70.0,
-              height: 40.0,
-              padding: EdgeInsets.all(12.0),
-              child: SvgPicture.asset('assets/icons/padlock.svg')
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30.0)
-            ),
-              hintText: '*******', labelText: 'Contraseña', ),
-          validator: _validatePsswd,
-                    ),
-                    SizedBox(
-          height: 40.0,
-                    ),
-                    MyBtn(
-          label: 'Ingresar',
-          fullWidth: true,
-          onPressed: _submit,
-          background: Color(0xff304ffe),
-          textColor: Colors.white,
-                    ),
-                  ],
-                ),
-            )),
-                  ],
-                ),
-            ),
+                  )),
+            ],
+          ),
+        ),
       )),
     );
   }
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:after_layout/after_layout.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -11,23 +12,32 @@ class _SplashPageState extends State<SplashPage> with AfterLayoutMixin {
   @override
   void initState() {
     super.initState();
-    
   }
+
   @override
   void afterFirstLayout(BuildContext context) {
-    Navigator.pushReplacementNamed(context, 'login');
+    _checkLogin();
+  }
+
+  _checkLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool auth = prefs.getBool('authenticated');
+    if (auth == null || !auth) {
+      Navigator.pushReplacementNamed(context, 'login');
+    } else {
+      Navigator.pushReplacementNamed(context, 'home');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(child: CupertinoActivityIndicator(
-                radius: 15,
-              ),)
-      ),
+          body: Center(
+        child: CupertinoActivityIndicator(
+          radius: 15,
+        ),
+      )),
     );
   }
-
-  
 }
