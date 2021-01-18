278836db7140b5cdc5f341db2a5282179c0e59ce
David Barcenas <daveepro@outlook.com>
2021-01-17 21:36:44 -0600

    feat(MoreTab): Change home content to more

--- lib/pages/tabs/home_tab.dart
+++ lib/pages/tabs/home_tab.dart
@@ -11,27 +11,8 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Avatar(),
-        SizedBox(
-          height: 20.0,
-        ),
-        Text('Bienvenido'),
-        Text(
-          'David Bárcenas',
-          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        MyBtn(
-          label: 'Aceptar',
-          onPressed: () =>
-              Navigator.pushNamed(context, PostPage.routeName),
-        ),
-      ]);
+    return Center(
+      child: Text('Home'),
+    );
   }
 }
--- lib/pages/tabs/more__tab.dart
+++ lib/pages/tabs/more__tab.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterui/widgets/avatar.dart';
+import 'package:flutterui/widgets/my_btn.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../post_page.dart';
 
 class MoreTab extends StatefulWidget {
   @override
@@ -17,9 +21,26 @@ class _MoreTabState extends State<MoreTab> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: CupertinoButton(
-      child: Text('Cerrar sesión'),
-      onPressed: _logOut,
-    ));
+        child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+          Avatar(),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text('Bienvenido'),
+          Text(
+            'David Bárcenas',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          MyBtn(
+            label: 'Aceptar',
+            onPressed: () => Navigator.pushNamed(context, PostPage.routeName),
+          ),
+        ]));
   }
 }
